b13fc4be8959289a866ef88aef0c2c98e7d8a0f4
pauloosmt <paulostaciano@gmail.com>
2026-01-06 13:05:35 -0300

    first commit

---
+++ src/main/java/com/br/apiToDoList/ApiToDoListApplication.java
@@ -0,0 +1,13 @@
+package com.br.apiToDoList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiToDoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiToDoListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/apiToDoList/ApiToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.apiToDoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
