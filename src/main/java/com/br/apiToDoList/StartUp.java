09b47d30718a46a250ccb9d1bb616fc6d2d0dc56
pauloosmt <paulostaciano@gmail.com>
2026-01-06 13:29:26 -0300

    refactor: rename "ApitoDoListApplication.java" to "StartUp.java"

--- src/main/java/com/br/apiToDoList/StartUp.java
+++ src/main/java/com/br/apiToDoList/StartUp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiToDoListApplication {
+public class StartUp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiToDoListApplication.class, args);
+		SpringApplication.run(StartUp.class, args);
 	}
 
 }
