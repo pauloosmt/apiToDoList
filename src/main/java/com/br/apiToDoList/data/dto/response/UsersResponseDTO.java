1ac64c3b65b13c7addb3643f4d83bc2643b79856
pauloosmt <paulostaciano@gmail.com>
2026-01-07 16:56:45 -0300

    feat: created UserService

--- src/main/java/com/br/apiToDoList/data/dto/response/UsersResponseDTO.java
+++ src/main/java/com/br/apiToDoList/data/dto/response/UsersResponseDTO.java
@@ -1,5 +1,7 @@
 package com.br.apiToDoList.data.dto.response;
 
+import com.br.apiToDoList.data.entity.Users;
+
 public record UsersResponseDTO(
 
     Long id,
@@ -7,5 +9,7 @@ public record UsersResponseDTO(
     String email,
     String password
 ) {
-
+    public UsersResponseDTO(Users users) {
+        this(users.getIdUser(), users.getName(), users.getEmail(), users.getPassword());
+    }
 }
