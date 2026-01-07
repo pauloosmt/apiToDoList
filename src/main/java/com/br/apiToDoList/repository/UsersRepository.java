ac8fed1686e6d6fa17c8e543d1a9a70d59c6fb38
pauloosmt <paulostaciano@gmail.com>
2026-01-07 13:03:05 -0300

    feat: created UsersRepository

---
+++ src/main/java/com/br/apiToDoList/repository/UsersRepository.java
@@ -0,0 +1,9 @@
+package com.br.apiToDoList.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.br.apiToDoList.data.entity.Users;
+
+@Repository
+public interface UsersRepository extends JpaRepository<Users, Long>{
+}
