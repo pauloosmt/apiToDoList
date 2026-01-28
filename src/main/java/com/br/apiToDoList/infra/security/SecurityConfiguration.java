10b08699ba9870f214f29c65073343eeaa81ad8a
pauloosmt <paulostaciano@gmail.com>
2026-01-28 15:54:16 -0300

    fix: added swagger config

--- src/main/java/com/br/apiToDoList/infra/security/SecurityConfiguration.java
+++ src/main/java/com/br/apiToDoList/infra/security/SecurityConfiguration.java
@@ -22,14 +22,17 @@ public class SecurityConfiguration {
     SecurityFilter securityFilter;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                .csrf(csrf -> csrf.disable())
                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                .authorizeHttpRequests(authorize -> authorize
                                     .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                                     .requestMatchers(HttpMethod.POST, "/auth/register" ).permitAll()
-                                    .requestMatchers(HttpMethod.DELETE, "/user").hasRole("ADMIN")
+                                    .requestMatchers(
+                                    "/v3/api-docs/**",
+                                                "/swagger-ui/**",
+                                                "/swagger-ui.html").permitAll()
                                     .anyRequest().authenticated()
                )
                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
